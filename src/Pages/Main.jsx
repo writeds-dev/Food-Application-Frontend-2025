bac11bf2ba7fff45937d28b3e8f49c4c7e2d285b
diya sharma <sdiya0523@gmail.com>
2025-12-14 20:15:50 +0530

    main changed

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -404,7 +404,8 @@ export default function Main(){
 </div>
 </div>
 
- <section className="bg-[#FFF1DE] py-20 mt-60">
+<div className="mt-60">
+ <section className="bg-[#FFF1DE] py-20 ">
   {/* Heading */}
   <div className="text-center max-w-4xl mx-auto">
     <p className="tracking-widest text-sm text-gray-600">
@@ -479,9 +480,9 @@ export default function Main(){
     ))}
   </div>
 </section>
+</div>
 
-
-
+<div></div>
 <section className="py-20 bg-white px-32">
       {/* Heading */}
       <div className="text-center mb-12">
